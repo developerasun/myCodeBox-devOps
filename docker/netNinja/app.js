03363a67164bc9b5a6d02928e86f0d3269e1a1fd
designerasun <asuntheawesome@gmail.com>
2022-02-18 18:38:53 +0900

    feat : add a mini project for docker

---
+++ docker/netNinja/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json([
+        {
+        'name' : 'jake', 
+        }, 
+        {
+        'name' : 'brian', 
+        }, 
+        {
+        'name' : 'paul', 
+        }, 
+    ])
+})
+
+app.listen(4000, ()=>console.log('listen at 4000'))
