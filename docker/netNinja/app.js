d634d7d0225579ccff4a7710c95c3baaa9f56204
designerasun <asuntheawesome@gmail.com>
2022-02-20 01:48:43 +0900

    feat : volume mapping for docker container

--- docker/netNinja/app.js
+++ docker/netNinja/app.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json([
         {
-        'name' : 'jake', 
+        'name' : 'not jake', 
         }, 
         {
         'name' : 'brian', 
